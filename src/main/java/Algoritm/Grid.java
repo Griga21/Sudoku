f5102de2ef257bffcbed480152d303f5fb620fa3
Griga <griga.k.r.21@gmail.com>
2023-11-20 20:24:46 +0300

    add swap separately colums and rows

--- src/main/java/Algoritm/Grid.java
+++ src/main/java/Algoritm/Grid.java
@@ -22,13 +22,17 @@ public class Grid {
         for (int i = 0; i < n * n; i++)
             for (int j = 0; j < n * n; j++)
                 copytable[i][j] = table[j][i];
-
+        for (int i = 0; i < n * n; i++) {
+            for (int j = 0; j < n * n; j++)
+                System.out.print(copytable[i][j] + " ");
+            System.out.println();
+        }
         return copytable;
     }
 
     public void showGrid() {
         table = transposing(table);
-        table = swapColumsArea(table);
+        table = swapRows(table);
         for (int i = 0; i < n * n; i++) {
             for (int j = 0; j < n * n; j++)
                 System.out.print(table[i][j] + " ");
@@ -36,7 +40,6 @@ public class Grid {
         }
     }
 
-    // Блочная смена по 3 столбцам
     public int[][] swapRowsArea(int[][] table) {
         int copyArea1, copyArea2;
         Random r = new Random();
@@ -102,27 +105,63 @@ public class Grid {
     public int[][] swapColums(int[][] table) {
         int copyArea1, copyArea2;
         Random r = new Random();
-        int x1 = r.nextInt(n*n - 1);
-
-        int x2 = r.nextInt(3);
-
-        while (true) {
-            if (x1 == x2)
-                x2 = r.nextInt(n);
-            else
-                break;
+        int x1 = r.nextInt(n * n - 1);
+        int x2 = 0;
+        if (x1 % n == 0) {
+            x2 = x1 - r.nextInt(1) + 1;
+        }
+        if (x1 % n == 1) {
+            while (true) {
+                x2 = r.nextInt(2);
+                if (x2 != 1) {
+                    x2 -= 1;
+                    x2 = x1 - x2;
+                    break;
+                }
+            }
+        }
+        if (x1 % n == 2) {
+            x2 = x1 - r.nextInt(1) - 2;
+        }
+        System.out.println(x1);
+        System.out.println(x2);
+        for (int i = 0; i < n * n; i++) {
+            copyArea1 = table[x1][i];
+            copyArea2 = table[x2][i];
+            table[x1][i] = copyArea2;
+            table[x2][i] = copyArea1;
         }
 
-
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < n * n; i++) {
-                copyArea1 = table[x1][i];
-                copyArea2 = table[x2][i];
-                table[x1][i] = copyArea2;
-                table[x2][i] = copyArea1;
+        return table;
+    }
+    public int[][] swapRows(int[][] table) {
+        int copyArea1, copyArea2;
+        Random r = new Random();
+        int x1 = r.nextInt(n * n - 1);
+        int x2 = 0;
+        if (x1 % n == 0) {
+            x2 = x1 - r.nextInt(1) + 1;
+        }
+        if (x1 % n == 1) {
+            while (true) {
+                x2 = r.nextInt(2);
+                if (x2 != 1) {
+                    x2 -= 1;
+                    x2 = x1 - x2;
+                    break;
+                }
             }
-            x1 += 1;
-            x2 += 1;
+        }
+        if (x1 % n == 2) {
+            x2 = x1 - r.nextInt(1) - 2;
+        }
+        System.out.println(x1);
+        System.out.println(x2);
+        for (int i = 0; i < n * n; i++) {
+            copyArea1 = table[x1][i];
+            copyArea2 = table[x2][i];
+            table[x1][i] = copyArea2;
+            table[x2][i] = copyArea1;
         }
 
         return table;
