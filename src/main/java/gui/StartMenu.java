914f55fa96a5e0c4600621de773fe765988106fb
Griga <griga.k.r.21@gmail.com>
2023-12-04 18:25:50 +0300

    add new features

--- src/main/java/gui/StartMenu.java
+++ src/main/java/gui/StartMenu.java
@@ -2,6 +2,8 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -10,20 +12,26 @@ public class StartMenu extends JFrame {
 
     private String configPath;
     private GuiItems guiItems = new GuiItems();
+    private Properties props = new Properties();
+    private GameMap gameMap;
 
     public StartMenu(String configPath) {
         this.configPath = configPath;
     }
 
-    public void showStrarMenu() {
+    public void showStrartMenu() {
+        gameMap = new GameMap(this, guiItems);
         loadProperties();
         drawStratApp();
-        this.setVisible(true);
+        addActions();
     }
 
-    private void loadProperties() {
+    public void updateMenu(){
+        drawStratApp();
+    }
 
-        Properties props = new Properties();
+
+    private void loadProperties() {
         FileInputStream in = null;
         try {
 
@@ -35,23 +43,41 @@ public class StartMenu extends JFrame {
             in.close();
         } catch (IOException e) {
             JOptionPane.showMessageDialog(this, "Неверный указанный путь к properties файлу",
-                    "Ошибка",	JOptionPane.ERROR_MESSAGE);
+                    "Ошибка", JOptionPane.ERROR_MESSAGE);
         }
 
+    }
+
+    private void drawStratApp() {
+        this.getContentPane().setBackground(Color.WHITE);
+        guiItems.getPicture().setIcon(guiItems.getImg());
+        guiItems.getPicture().setBounds(Integer.parseInt((props.getProperty("frameWidth"))) / 2 - guiItems.getImg().getIconWidth() / 2,
+                40, guiItems.getImg().getIconWidth(),
+                guiItems.getImg().getIconHeight());
+        this.add(guiItems.getPicture());
+        guiItems.getStartButton().setBounds(Integer.parseInt((props.getProperty("frameWidth"))) / 2 - 40,
+                guiItems.getImg().getIconHeight() + 50, 80, 40);
+        this.add(guiItems.getStartButton());
+        guiItems.getLevel().setBounds(Integer.parseInt((props.getProperty("frameWidth"))) / 2 - 80,
+                guiItems.getImg().getIconHeight() + 110, 160, 40);
+        this.add(guiItems.getLevel());
+
         this.setTitle("Судоку");
-        this.setIconImage(new ImageIcon("C:\\Work dir\\Sudoku-master\\src\\main\\resources\\logo.png").getImage());
+        this.setIconImage(new ImageIcon("logo.png").getImage());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
+        this.setVisible(true);
     }
 
-    private void drawStratApp(){
-        guiItems.getPicture().setIcon(guiItems.getImg());
-        guiItems.getPicture().setBounds(410, 55, guiItems.getImg().getIconWidth(), guiItems.getImg().getIconHeight());
-        this.add(guiItems.getPicture());
-        guiItems.getStartButton().setBounds(10, 10, 80, 40);
-        this.add(guiItems.getStartButton());
-        guiItems.getLevel().setBounds(50, 60, 80, 40);
-        this.add(guiItems.getLevel());
+    private void addActions() {
+        guiItems.getStartButton().addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        gameMap.drawGameMap();
+                    }
+                }
+        );
     }
 }
